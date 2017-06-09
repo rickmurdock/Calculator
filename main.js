b67a4dd1616e92500273793315a5077b224f7643
rickmurdock <rick.murdock@live.com>
2017-06-09 10:02:02 -0500

    Initial calculator setup

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
+
+var display = "";
+var total = 0;
+var operator = "";
+var number1 = 0;
+var number2 = 0;
+
 function clearAnswer() {
+    // total = 0;
     var answerButton = document.querySelector('#answer');
+    display = " ";
     answerButton.value = " ";
+}
+
+function displayTotal() {
+    // parse string here to create total
+    // strip off any trailing operators
+}
+
+function updateDisplay(keyPressed) {
+    var answerButton = document.querySelector('#answer');
+
+    if (!isNaN(keyPressed) || !isNaN(display.substr(display.length-1))) {
+        display = display + keyPressed;
+        answerButton.value = display;
+    }
 }
