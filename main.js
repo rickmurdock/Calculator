ecd6a7bff713e4a471ec54fb66a1ff3a92d46d1f
rickmurdock <rick.murdock@live.com>
2017-06-09 21:46:26 -0500

    Reduced calculations with eval statement.

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
 
 var display = "";
-var total = 0;
-var operator = "";
-var number1 = 0;
-var number2 = 0;
+var answerButton = document.querySelector('#answer');
 
 function clearAnswer() {
-    // total = 0;
-    var answerButton = document.querySelector('#answer');
-    display = " ";
+    display = "";
     answerButton.value = " ";
 }
 
-function displayTotal() {
-    // parse string here to create total
-    // strip off any trailing operators
-}
-
 function updateDisplay(keyPressed) {
-    var answerButton = document.querySelector('#answer');
-
-    if (!isNaN(keyPressed) || !isNaN(display.substr(display.length-1))) {
+    if (!isNaN(keyPressed) || ((display.length > 0) && !isNaN(display.substr(display.length-1)))) {
         display = display + keyPressed;
         answerButton.value = display;
     }
+}
+
+function displayTotal() {
+
+ //strip off trailing operators 
+
+    // if (isNaN(display)) {
+        var x = eval(display);
+        console.log(x);
+        answerButton.value = x;
+    // }
 }
