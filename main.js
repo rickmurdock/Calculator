acbf4b767d39628d7b7450da0f4d060599796b67
rickmurdock <rick.murdock@live.com>
2017-06-13 21:57:56 -0500

    Fixed issue with decimal

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 
 var display = "";
 var answerButton = document.querySelector('#answer');
+var decimalFound = false;
 
 function clearAnswer() {
     display = "";
     answerButton.value = " ";
+    decimalFound = false;  
 }
-
+  
 function updateDisplay(keyPressed) {
-    if (!isNaN(keyPressed) || ((display.length > 0) && !isNaN(display.substr(display.length-1)))) {
-        display = display + keyPressed;
-        answerButton.value = display;
+
+    if ((!isNaN(keyPressed) || (keyPressed == '.' && decimalFound == false)) ||
+        (display.length > 0 && !isNaN(display.substr(display.length-1)) )) {
+        
+        if (keyPressed != '.' || ((keyPressed == '.') && !decimalFound)) {
+            display = display + keyPressed;
+            answerButton.value = display;
+        }
+       
+        if (keyPressed == '.') { 
+            decimalFound = true;
+        } else if (isNaN(keyPressed)) {
+            decimalFound = false;
+        } 
     }
 }
 
